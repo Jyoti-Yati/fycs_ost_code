6759b14d1f42ef933d3b904a40240f787423cb54
Jyoti-Yati <115882595+Jyoti-Yati@users.noreply.github.com>
2022-11-21 08:47:12 +0530

    Add files via upload

---
+++ pythonOST.py
@@ -0,0 +1,18 @@
+#Python 2 code based on multiplication and addition
+
+#Taking the variable as num1 and num2
+
+num1=int(input("Enter the first number:"))
+num2=int(input("Enter the second number:"))
+
+#performing the multiplication between two numbers :
+
+result=num1*num2
+print("The multiple of two number is",result)
+
+
+#performing the addition between two numbers :
+
+result=num1+num2
+print("The addition of two number is",result)
+
